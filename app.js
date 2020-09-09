11cf8a0e9f8a31f0c7f13a11f49367de752a6767
Валерий <macuser@iMac-Valera.local>
2020-09-09 12:29:42 +0300

    Add table + res

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+  //alert("check console.log");
   function listOfUsers() {
       const url = 'https://jsonplaceholder.typicode.com/users';
 
+
       fetch(url).then(req => {
         console.log('request: ', req);
-        req.json();
-      }).then(data => console.log(data));
+        return req.json();
+      }).then(data => {
+    for(let i=0; i<data.length; i++){
+      console.log(data[i]);
+    }
+  });
 
-  }
-
-  listOfUsers();
+}//listOfUsers
 
 
 
+  //listOfUsers();
 
 });//End of DOMContentLoaded
