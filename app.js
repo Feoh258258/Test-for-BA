f1c6bcbf4f67f68577644a6816bb408466534291
Валерий <macuser@iMac-Valera.local>
2020-09-11 13:39:07 +0300

    Resp
    mobile

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
       let theadTd = document.createElement('td');
       theadTd.innerText = "sort";
       theadTd.classList.add('tdSort');
+      theadTd.onclick = ()=>{
+        sortTable()
+      }
       theadTr.appendChild(theadTd);
     }
     
@@ -37,6 +40,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
           if(tableParams.indexOf(name)>=0){
             let td = document.createElement('td');
             td.innerText = users[i][name];
+            td.classList.add('tdElement');
             tr.appendChild(td);
           }
         }
@@ -52,10 +56,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 function openModal(street, city, zip) {
   let modal = document.createElement('div');
   let closeModal = document.createElement('div');
+  let overlayModal = document.createElement('div');
   let pZip = document.createElement('p');
   let pStreet = document.createElement('p');
   let pCity = document.createElement('p');
   modal.classList.add('modal-st');
+  overlayModal.classList.add('overlayModal-st');
   closeModal.classList.add('close-modal-st');
   pZip.innerText = "ZIP Code: " + zip;
   pStreet.innerText = "Street: " + street;
@@ -63,6 +69,7 @@ function openModal(street, city, zip) {
   closeModal.innerText = "закрыть";
   closeModal.onclick = ()=>{
     modal.style.display = "none";
+    overlayModal.style.display = "none";
     document.body.style.background = "white";
     }
 
@@ -70,11 +77,33 @@ function openModal(street, city, zip) {
   modal.appendChild(pZip);
   modal.appendChild(pStreet);
   modal.appendChild(pCity);
+  document.body.appendChild(overlayModal);
   document.body.style.background = "grey";
   document.body.appendChild(modal);
 }
 
-
+function sortTable() {
+  let table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementsByClassName("table");
+  switching = true;
+  while (switching) {
+    switching = false;
+    rows = document.getElementsByTagName("TR");
+    for (i = 1; i < (rows.length - 1); i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[0];
+      y = rows[i + 1].getElementsByTagName("TD")[0];
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
 
 
 
